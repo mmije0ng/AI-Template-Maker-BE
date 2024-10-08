4e70a3f05643f4482efb2c1408e1ca66b569509a
mmije0ng <victon777@naver.com>
2024-10-08 14:27:26 +0900

    Init: 개발환경 세팅

---
+++ src/test/java/com/backend/sparkle/SparkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sparkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
