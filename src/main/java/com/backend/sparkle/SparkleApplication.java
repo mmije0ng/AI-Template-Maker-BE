9ddc89703f04a5361698df8361fc3e3fe7e4093a
mmije0ng <victon777@naver.com>
2024-10-12 01:49:14 +0900

    Feat: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/backend/sparkle/SparkleApplication.java
+++ src/main/java/com/backend/sparkle/SparkleApplication.java
@@ -2,7 +2,12 @@ package com.backend.sparkle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SparkleApplication {
 
