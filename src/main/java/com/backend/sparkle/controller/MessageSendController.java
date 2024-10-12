b8062d0bcfc8227effe0e64d051677d65924678b
mmije0ng <victon777@naver.com>
2024-10-13 00:47:40 +0900

    Feat: 문자 보내기 페이지 API 생성 (추가 필요)

--- src/main/java/com/backend/sparkle/controller/MessageSendController.java
+++ src/main/java/com/backend/sparkle/controller/MessageSendController.java
@@ -2,6 +2,7 @@ package com.backend.sparkle.controller;
 
 import com.backend.sparkle.dto.CommonResponse;
 import com.backend.sparkle.dto.MessageDto;
+import com.backend.sparkle.dto.UserDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
@@ -34,13 +35,91 @@ public class MessageSendController {
 
             // 이미지 생성 응답 객체 생성
             MessageDto.ImageGenerateResponseDto responseDto = MessageDto.ImageGenerateResponseDto.builder()
-                    .imageUrlList(imageUrlList)
+                    .imageURLList(imageUrlList)
                     .build();
 
             return CommonResponse.success("이미지 생성 성공", responseDto);
         } catch (Exception e) { // 예외 처리 필요
             log.error("이미지 생성 실패: {}", e.getMessage());
             return CommonResponse.fail(HttpStatus.NOT_FOUND, "이미지 생성 실패");
+        }
+    }
+
+    // 템플릿 기능을 통해 완성된 이미지 + 텍스트를 뿌리오 API와 연동하여 문자 전송
+    @Operation(summary = "템플릿 및 발송화면",
+            description = "생성된 이미지 3장 중 사용자가 1장 선택한 후 템플릿 및 발송화면으로 전환")
+    @GetMapping("/template/{userId}")
+    public CommonResponse<?> getTemplateSendPage(@PathVariable Long userId, @RequestBody MessageDto.TemplateRequestDto requestDto) {
+        try {
+            log.info("템플릿 및 발송화면 요청 userId: {}", userId);
+
+            List<String> sendPhoneNumbers = new ArrayList<>(); // 발신자 목록
+            sendPhoneNumbers.add("010-0000-0000");
+            sendPhoneNumbers.add("010-1234-5678");
+            sendPhoneNumbers.add("010-5678-1234");
+
+            List<String> addressListNames = new ArrayList<>(); // 주소록 별칭 목록
+            addressListNames.add("한성대 주소록");
+            addressListNames.add("김선생 수학 학원 주소록");
+
+            MessageDto.TemplateResponseDto responseDto = MessageDto.TemplateResponseDto.builder()
+                    .inputMessage(requestDto.getInputMessage())
+                    .selectedImageURL(requestDto.getSelectedImageURL())
+                    .sendPhoneNumbers(sendPhoneNumbers)
+                    .addressListNames(addressListNames)
+                    .build();
+
+            return CommonResponse.success("템플릿 및 발송화면 요청 성공", responseDto);
+        } catch (Exception e) { // 예외 처리 필요
+            log.error("템플릿 및 발송화면 요청 실패: {}", e.getMessage());
+            return CommonResponse.fail(HttpStatus.NOT_FOUND, "템플릿 및 발송화면 요청 실패");
+        }
+    }
+
+    // 이미지 + 텍스트 문자 발송하기
+    // 프론트 => 백으로 이미지 + 텍스트 넘겨주면 백에서 뿌리오 API와 연동하여 메시지를 보내야 함 (별도의 로직 필요)
+    // 뿌리오 API와 연동하여 메시지를 보낸 후 프론트로 응답 전송
+    @Operation(summary = "이미지 + 텍스트 문자 발송", description = "템플릿 기능을 통해 완성된 이미지 + 텍스트 문자 전송 요청")
+    @PostMapping("/send/{userId}")
+    public CommonResponse<?> sendMessage(@PathVariable Long userId, @RequestBody MessageDto.SendRequestDto requestDto) {
+        try {
+            log.info("이미지 + 텍스트 문자 발송 요청 userId: {}", userId);
+
+            MessageDto.SendResponseDto responseDto = MessageDto.SendResponseDto.builder()
+                    .userId(userId)
+                    .completeImageURL("https://i.pinimg.com/564x/f0/e0/9c/f0e09cba73d689fc2c0ef01bbbbeae1a.jpg")
+                    .sendDateTime("2024-10-13")
+                    .sendPhoneNumber("010-0000-0000")
+                    .addressListName("한성대 주소록")
+                    .build();
+
+            return CommonResponse.success("이미지 + 텍스트 문자 발송 요청 성공", responseDto);
+        } catch (Exception e) { // 예외 처리 필요
+            log.error("이미지 + 텍스트 문자 발송 요청 실패: {}", e.getMessage());
+            return CommonResponse.fail(HttpStatus.NOT_FOUND, "이미지 + 텍스트 문자 발송 요청 실패");
+        }
+    }
+
+    // 문자 테스트 발송
+    // 수정 필요
+    @Operation(summary = "이미지 + 텍스트 문자 테스트 발송", description = "템플릿 기능을 통해 완성된 이미지 + 텍스트 문자 테스트 발송 요청")
+    @PostMapping("/send/test/{userId}")
+    public CommonResponse<?> sendTestMessage(@PathVariable Long userId /*, @RequestBody MessageDto.SendRequestDto requestDto*/) {
+        try {
+//            log.info("이미지 + 텍스트 문자 발송 요청 userId: {}", userId);
+//
+//            MessageDto.SendResponseDto responseDto = MessageDto.SendResponseDto.builder()
+//                    .userId(userId)
+//                    .completeImageURL("https://i.pinimg.com/564x/f0/e0/9c/f0e09cba73d689fc2c0ef01bbbbeae1a.jpg")
+//                    .sendDateTime("2024-10-13")
+//                    .sendPhoneNumber("010-0000-0000")
+//                    .addressListName("한성대 주소록")
+//                    .build();
+
+            return CommonResponse.success("이미지 + 텍스트 문자 테스트 발송 요청 성공", userId);
+        } catch (Exception e) { // 예외 처리 필요
+            log.error("이미지 + 텍스트 문자 테스트 발송 요청 실패: {}", e.getMessage());
+            return CommonResponse.fail(HttpStatus.NOT_FOUND, "이미지 + 텍스트 문자 테스트 발송 요청 실패");
         }
     }
 }
--- src/main/java/com/backend/sparkle/dto/MessageDto.java
+++ src/main/java/com/backend/sparkle/dto/MessageDto.java
@@ -30,7 +30,7 @@ public class MessageDto {
     // 이미지 생성 응답 dto
     public static class ImageGenerateResponseDto {
         // 생성된 3장의 이미지 URL 경로 리스트
-        private List<String> imageUrlList;
+        private List<String> imageURLList;
     }
 
     @Getter
@@ -38,9 +38,56 @@ public class MessageDto {
     @AllArgsConstructor
     // 문자 내역 조회 응답 dto
     public static class HistoryResponseDto {
-        private String imageUrl; // 이미지 URL 경로
+        private String imageURL; // 이미지 URL 경로
         private String sendDate; // 발송일자
         private String sendPhoneNumber; // 발신번호
         private String addressName; // 주소록 별칭
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    // 템플릿 및 발송 화면 요청 dto
+    public static class TemplateRequestDto {
+        private String inputMessage; // 입력된 메시지
+        private String selectedImageURL; // 선택된 이미지 URL 경로
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    // 문자 내역 조회 응답 dto
+    public static class TemplateResponseDto {
+        private String inputMessage; // 입력된 메시지
+        private String selectedImageURL; // 선택된 이미지 URL 경로
+        private List<String> sendPhoneNumbers; // 발신번호 목록
+        private List<String> addressListNames; // 수신번호 주소록 별칭 목록
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    // 이미지 + 텍스트 문자 전송 요청 dto
+    public static class SendRequestDto {
+        private String inputMessage; // 입력된 메시지
+        private String completeImageURL; // 템플릿 기능을 통해 완성된 이미지 URL 경로
+        private String sendPhoneNumber; // 발신번호
+        private String addressListName; // 주소록 별칭
+        private int sendType; // 0이면 즉시 발송, 1이면 예약 발송
+        private String sendDateTime; // 발송 날짜 및 시간
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    // 이미지 + 텍스트 문자 전송 응답 dto
+    public static class SendResponseDto {
+        private Long userId; // 사용자 PK
+        private String completeImageURL; // 템플릿 기능을 통해 완성된 이미지 URL 경로 = 발송 이미지
+        private String sendDateTime; // 발송 날짜 및 시간
+        private String sendPhoneNumber; // 발신번호
+        private String addressListName; // 주소록 별칭
     }
 }
