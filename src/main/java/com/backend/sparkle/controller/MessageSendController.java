5243f9d8571175b7b8ce838819cec2f86c06ef6d
mmije0ng <victon777@naver.com>
2024-10-20 01:35:23 +0900

    Fix: 메시지 dto 필드 수정

--- src/main/java/com/backend/sparkle/controller/MessageSendController.java
+++ src/main/java/com/backend/sparkle/controller/MessageSendController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/backend/sparkle/dto/MessageDto.java
+++ src/main/java/com/backend/sparkle/dto/MessageDto.java
@@ -19,12 +19,7 @@ public class MessageDto {
     @Schema(description = "이미지 생성 요청 정보")
     public static class ImageGenerateRequestDto {
 
-        @Schema(description = "발송 목적 및 내용", example =
-                """
-                한성대 피자짱 가게에서 방문 포장 시 50% 할인을 진행합니다.
-                위 광고 문자를 결제 시 직원분에게 보여주시면 됩니다.
-                일부 품목에 한해서 할인이 제한될 수 있습니다.
-                감사합니다.""")
+        @Schema(description = "발송 목적 및 내용", example = "피자 광고에 관한 이미지를 만들고 싶다.")
         private String inputMessage;
 
         @Schema(description = "분위기 키워드", example = "차분한 분위기")
@@ -71,18 +66,6 @@ public class MessageDto {
     @Getter
     @Builder
     @AllArgsConstructor
-    @NoArgsConstructor
-    @Schema(description = "템플릿 및 발송 화면 요청 정보")
-    public static class TemplateRequestDto {
-
-        @Schema(description = "선택된 이미지 URL 경로",
-                example = "https://i.pinimg.com/564x/f0/e0/9c/f0e09cba73d689fc2c0ef01bbbbeae1a.jpg")
-        private String selectedImageURL;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
     @Schema(description = "템플릿 및 발송 화면 응답 정보")
     public static class TemplateResponseDto {
 
@@ -104,8 +87,13 @@ public class MessageDto {
     @NoArgsConstructor
     @Schema(description = "이미지 + 텍스트 문자 전송 요청 정보")
     public static class SendRequestDto {
-        @Schema(description = "발송 메시지 ", example =
-                "피자 광고에 관한 이미지를 만들어줘.")
+
+        @Schema(description = "발송 메시지", example =
+                """
+                한성대 피자짱 가게에서 방문 포장 시 50% 할인을 진행합니다.
+                위 광고 문자를 결제 시 직원분에게 보여주시면 됩니다.
+                일부 품목에 한해서 할인이 제한될 수 있습니다.
+                감사합니다.""")
         private String sendMessage;
 
         @Schema(description = "템플릿 기능을 통해 완성된 이미지 URL 경로",
@@ -125,33 +113,33 @@ public class MessageDto {
         private String sendDateTime;
     }
 
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @Schema(description = "이미지 + 텍스트 문자 전송 응답 정보")
-    public static class SendResponseDto {
-        @Schema(description = "사용자 PK", example = "1")
-        private Long userId;
-
-        @Schema(description = "입력된 메시지", example =
-                """
-                한성대 피자짱 가게에서 방문 포장 시 50% 할인을 진행합니다.
-                위 광고 문자를 결제 시 직원분에게 보여주시면 됩니다.
-                일부 품목에 한해서 할인이 제한될 수 있습니다.
-                감사합니다.""")
-        private String inputMessage; // 입력된 메시지
-
-        @Schema(description = "템플릿 기능을 통해 완성된 이미지 URL 경로, 즉 발송 이미지 URL 경로",
-                example = "https://i.pinimg.com/564x/f0/e0/9c/f0e09cba73d689fc2c0ef01bbbbeae1a.jpg")
-        private String completeImageURL;
-
-        @Schema(description = "발송일자", example = "2024-10-13")
-        private String sendDateTime; // 발송 날짜 및 시간
-
-        @Schema(description = "발신번호", example = "010-1234-5678")
-        private String sendPhoneNumber;
-
-        @Schema(description = "주소록 별칭", example = "김선생 영어학원 주소록")
-        private String addressName;
-    }
+//    @Getter
+//    @Builder
+//    @AllArgsConstructor
+//    @Schema(description = "이미지 + 텍스트 문자 전송 응답 정보")
+//    public static class SendResponseDto {
+//        @Schema(description = "사용자 PK", example = "1")
+//        private Long userId;
+//
+//        @Schema(description = "입력된 메시지", example =
+//                """
+//                한성대 피자짱 가게에서 방문 포장 시 50% 할인을 진행합니다.
+//                위 광고 문자를 결제 시 직원분에게 보여주시면 됩니다.
+//                일부 품목에 한해서 할인이 제한될 수 있습니다.
+//                감사합니다.""")
+//        private String sendMessage; // 입력된 메시지
+//
+//        @Schema(description = "템플릿 기능을 통해 완성된 이미지 URL 경로, 즉 발송 이미지 URL 경로",
+//                example = "https://i.pinimg.com/564x/f0/e0/9c/f0e09cba73d689fc2c0ef01bbbbeae1a.jpg")
+//        private String completeImageURL;
+//
+//        @Schema(description = "발송일자", example = "2024-10-13")
+//        private String sendDateTime; // 발송 날짜 및 시간
+//
+//        @Schema(description = "발신번호", example = "010-1234-5678")
+//        private String sendPhoneNumber;
+//
+//        @Schema(description = "주소록 별칭", example = "김선생 영어학원 주소록")
+//        private String addressName;
+//    }
 }
